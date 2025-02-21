72a5e35f543343cc7303846351fe82680de954eb
Alexander Klostermann <akloster@mi-r8-p3.s19.be>
2025-02-21 18:24:02 +0100

    fix: added check for 0 division

--- src/rendering.c
+++ src/rendering.c
@@ -6,19 +6,23 @@
 /*   By: akloster <akloster@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 17:28:40 by akloster          #+#    #+#             */
-/*   Updated: 2025/01/18 21:03:03 by akloster         ###   ########.fr       */
+/*   Updated: 2025/02/21 18:23:21 by akloster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	put_pixel(t_img *frame, int x, int y, unsigned int color)
+static int	put_pixel(t_img *frame, int x, int y, unsigned int color)
 {
 	if (x >= 0 && y >= 0 && x <= WIN_WIDTH && y <= WIN_HEIGHT
 		&& ((y * WIN_WIDTH + x) <= (WIN_WIDTH * WIN_HEIGHT - 1)))
 		frame->data[y * WIN_WIDTH + x] = color;
 	else
+	{
 		printf("(%d %d) %x\n", x, y, color); // <<TAKE out
+		return (1);
+	}
+	return (0);
 }
 
 static unsigned int	get_color(t_data *data, t_ray *ray, int x, int y)
@@ -47,7 +51,7 @@ static void	calibrate_texture(t_data *data, t_wall *wall, t_ray *ray)
 	if (ray->side && ray->cast[Y] < 0)
 		wall->tex_X = TEX_WIDTH - wall->tex_X - 1;
 	wall->step = 1.0 * TEX_HEIGHT / wall->h;
-	wall->pos = (wall->start - WIN_HEIGHT / 2 + wall->h / 2) * wall->step;	
+	wall->pos = (wall->start - WIN_HEIGHT / 2 + wall->h / 2) * wall->step;
 }
 
 void	render_column(t_data *data, t_img *frame, t_ray *ray, int x)
@@ -56,6 +60,8 @@ void	render_column(t_data *data, t_img *frame, t_ray *ray, int x)
 	int		y;
 
 	wall.h = (int) (WIN_HEIGHT / ray->perp_dist); //hunt down perp_dist
+	if (ray->perp_dist == 0)
+		wall.h = WIN_HEIGHT;
 	wall.start = -wall.h / 2 + WIN_HEIGHT / 2;
 	if (wall.start < 0)
 		wall.start = 0;
@@ -68,18 +74,21 @@ void	render_column(t_data *data, t_img *frame, t_ray *ray, int x)
 	{
 		wall.tex_Y = (int) wall.pos & (TEX_HEIGHT - 1);
 		wall.pos += wall.step;
-		put_pixel(frame, x, y, get_color(data, ray, wall.tex_X, wall.tex_Y));
+		if (put_pixel(frame, x, y, get_color(data, ray, wall.tex_X, wall.tex_Y)))
+			printf("y in render = %d\n", y);
+		
 	}
 	y = -1;
 //	if (!x)
 //		printf("wall %d ray->perp_disti %f start %d\n", wall.h, ray->perp_dist, wall.start);
 	while (++y < wall.start)
-	{
-		put_pixel(frame, x, y, data->info->c_sky_hex);
-	}
+		put_pixel(frame, x, y, data->info->c_sky_hex)
 	y = wall.end;
 	while (++y < WIN_HEIGHT)
-		put_pixel(frame, x, y, data->info->c_floor_hex);
+	{
+		if (put_pixel(frame, x, y, data->info->c_floor_hex))
+			printf("y in after wall = %d\n", y);
+	}
 //	if (!x)
 //		printf("!rendering\n");
 }
